22b2c72098749bde82c8c19393aa63616053917e
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2014-10-01 17:59:35 -0700

    Sign when making release dmg

--- mkdmg.sh
+++ mkdmg.sh
@@ -3,7 +3,7 @@ set -e
 
 VOLNAME=Vocabulist
 APPNAME=Vocabulist
-TARGET_BUILD_DIR=/Users/gonzo/Projects/vocabulist/build/Release
+TARGET_BUILD_DIR=$(PWD)/build/Release
 VERSION=`grep -A 1 CFBundleShortVersionString Vocabulist/Vocabulist-Info.plist | tail -1 | sed 's/[^0-9]*>//' | sed 's/<.*//'`
 
 rm -Rf build
@@ -23,6 +23,13 @@ hdiutil create -size 15000k -volname "$VOLNAME" -attach -fs HFS+ "$TARGET_BUILD_
 
 cp -R "$TARGET_BUILD_DIR/$APPNAME.app" "/Volumes/$VOLNAME/"
 
+sign="Mac Developer: Oleksandr Tymoshenko (JVN4Z6SEGD)"
+
+echo --sign app--
+codesign --preserve-metadata=identifier,entitlements,resource-rules,requirements --force --deep --verify --verbose --sign "$sign" "/Volumes/$VOLNAME/$APPNAME.app"
+ls -la "/Volumes/$VOLNAME/"
+
+codesign --deep-verify --display -vvv "/Volumes/$VOLNAME/$APPNAME.app"
 
 hdiutil detach "/Volumes/$VOLNAME"
 
